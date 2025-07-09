6f7c2cfcc73b3653127241eea6b9b0e9769bff7a
bigit22 <koresh.yar@mail.ru>
2025-07-09 21:29:54 +0700

    models for sql create

---
+++ core/models/complaint.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Enum
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+
+from core.entities.enums import StatusEnum, SentimentEnum, CategoryEnum
+from core.models.base import Base
+
+
+class ComplaintModel(Base):
+    __tablename__ = 'complaints'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    text: Mapped[str] = mapped_column(nullable=False)
+    status: Mapped[StatusEnum] = mapped_column(Enum(StatusEnum), default=StatusEnum.open)
+    timestamp: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    sentiment: Mapped[SentimentEnum] = mapped_column(Enum(SentimentEnum))
+    category: Mapped[CategoryEnum] = mapped_column(Enum(CategoryEnum))
